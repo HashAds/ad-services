965070d3c022d98acc4007c8dea3ee202b7de750
Robert Kainz <robertkainz@Roberts-MacBook-Pro.local>
2018-10-15 18:00:35 -0500

    disable cors

--- src/main/java/com/hashads/hashads/AppConfiguration.java
+++ src/main/java/com/hashads/hashads/AppConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class AppConfiguration {
@@ -18,5 +21,15 @@ public class AppConfiguration {
         return p;
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
 
 }
